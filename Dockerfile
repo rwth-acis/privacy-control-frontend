a6f56eb3318fe52489bb8b7e09d61c7f919841bd
Boris Jovanović <bokisjov@gmail.com>
2022-06-22 13:31:26 +0200

    dockerisation

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+#stage 1
+FROM node:14.17.4 as node
+WORKDIR /app
+COPY . .
+RUN npm install
+RUN npm run build --prod
+
+#stage 2
+FROM nginx:alpine
+COPY --from=node /app/dist/privacy-control-service-frontend /usr/share/nginx/html
+EXPOSE 80
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,21 @@
+const ROOT_URL = "http://localhost:32009/pieces/";
+
 export const environment = {
-  production: true
+  production: true,
+  urlRoot: ROOT_URL,
+  urlInit: ROOT_URL + "init/",
+  urlRoles: ROOT_URL + "user-roles",
+  urlPurposeList: ROOT_URL + "purpose-list/",
+  urlConsentOverview: ROOT_URL + "consent/",
+  urlCollectedData: ROOT_URL + "collected-data/",
+  urlManagerOverview: ROOT_URL + "manager/",
+  urlAllManagers: ROOT_URL + "managers",
+  urlManagerCreate: ROOT_URL + "register/manager",
+  urlServiceCreate: ROOT_URL + "register/service",
+  urlCourseCreate: ROOT_URL + "register/course",
+  urlPurposeInCourseCreate: ROOT_URL + "register/purpose-in-course/",
+  urlPurposesInCourseGet: ROOT_URL + "purpose-in-course/",
+  urlStudentsInCourseGet: ROOT_URL + "student-in-course/",
+  urlStudentsInCourseCreate: ROOT_URL + "register/student-in-course/",
+  urlGetL2PServices: ROOT_URL + "get-l2p-services",
 };
